2d6fc471632f210ff0fc267d5dc9abe9c0664224
Marco Antonio Luna Villarce <mrclv@MacBook-Air-de-Mrc.local>
2024-05-16 11:52:16 -0600

    First commit. All is runnin well

---
+++ SCADA_A.Datos/Mapping/ProduccionPintura/OrderPVMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SCADA_A.Entidades.ProduccionPintura;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCADA_A.Datos.Mapping.ProduccionPintura
+{
+    public class OrderPVMap : IEntityTypeConfiguration<OrderPV>
+    {
+        public void Configure(EntityTypeBuilder<OrderPV> builder)
+        {
+            builder.ToTable("PL_OrderPV")
+                .HasKey(p => p.OrderID);
+        }
+    }
+}
---
+++ SCADA_A.Entidades/ProduccionPintura/OrderPV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SCADA_A.Entidades.ProduccionPintura
+{
+	public class OrderPV
+	{
+		public Guid OrderID { get; set; }
+		public string OrderName { get; set; }
+		public int LabelID { get; set; }
+		public string? Comment { get; set; }
+	}
+}
+
